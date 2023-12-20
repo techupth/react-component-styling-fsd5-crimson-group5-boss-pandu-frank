53137215843f80bd0673b2bed363f85b99d9a779
nattawat <frank.nattawat@hotmail.com>
2023-12-20 22:28:58 +0700

    feat:complete ex

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,100 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import frown from "../pic/frown.png";
+import check from "../pic/check.png";
+import circle from "../pic/circle.png";
+import triangle from "../pic/triangle.png";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 2px;
+            width: 500px;
+            height: 80px;
+            background-color: #f9c8c8;
+            font-size: 16px;
+            border-radius: 4px;
+            color: black;
+            font-weight: bold;
+            flex-shrink: 0;
+            margin: 10px;
+          `}
+        >
+          <img src={frown} />
+          {props.text}
+        </button>
+      </div>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 2px;
+            width: 500px;
+            height: 80px;
+            background-color: #f9d9c8;
+            font-size: 16px;
+            border-radius: 4px;
+            color: black;
+            font-weight: bold;
+            flex-shrink: 0;
+            margin: 10px;
+          `}
+        >
+          <img src={triangle} />
+          {props.text}
+        </button>
+      </div>
+    );
+  } else if (props.type === "info") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 2px;
+            width: 500px;
+            height: 80px;
+            background-color: #f9ebc8;
+            font-size: 16px;
+            border-radius: 4px;
+            color: black;
+            font-weight: bold;
+            flex-shrink: 0;
+            margin: 10px;
+          `}
+        >
+          <img src={circle} />
+          {props.text}
+        </button>
+      </div>
+    );
+  } else if (props.type === "success") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 2px;
+            width: 500px;
+            height: 80px;
+            background-color: #cef7cd;
+            font-size: 16px;
+            border-radius: 4px;
+            color: black;
+            font-weight: bold;
+            flex-shrink: 0;
+            margin: 10px;
+          `}
+        >
+          <img src={check} />
+          {props.text}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,45 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 24px;
+            background-color: #074ee8;
+            font-size: 16px;
+            border-radius: 4px;
+            color: white;
+            font-weight: bold;
+            margin: 10px;
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div>
+        <button
+          css={css`
+            padding: 24px;
+            background-color: #07a4e8;
+            font-size: 16px;
+            border-radius: 4px;
+            color: white;
+            font-weight: bold;
+            margin: 10px;
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  }
+  return <h1>Noting</h1>;
+}
+
+export default Button;
